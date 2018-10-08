6b6e3dbc7a4c15cbb76092dbed3c640be9cea0bb
vincepay <varghapayandeh@gmail.com>
2018-10-08 15:03:34 -0600

    fixed teh Beers component

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -6,13 +6,27 @@ class Beers extends React.Component {
   state = {beers:[]}
 
   componentDidMount() {
+    // debugger // just to see how github shows modified lines
     axios.get('/api/all_beers')
       // Just to ensure that beers wont be empty
       // .then( res => this.setState({ beers: [ {id:1,name:'TestBeer'}, ...res.data.entries ] }) )
       .then( res => {
         this.setState({ beers: res.data.entries }) 
-      // debugger // just to see how github shows modified lines
+        // this.setBeer(res)
       })
+  }
+
+  setBeer = (res) => this.setState({beers: res.data.entries})
+
+  showBeers = () => { 
+    const { beers } = this.state
+    return beers.map( b => (
+      <li key={b.id}>
+        <Link to={`/beers/${b.name}`} style={{color:'red'}}>
+          {b.name}
+        </Link>
+      </li>
+    ))
   }
 
   render() {
@@ -22,13 +36,16 @@ class Beers extends React.Component {
         {/* <h1 style='color:green'>Beer List</h1> */}
         <h1 style={{color:'red'}}>Beer List</h1>
         <ul>
-          { beers.map( b => {
+          {/* {this.showBeers()} */}
+          {
+            beers.map( b => (
               <li key={b.id}>
                 <Link to={`/beers/${b.name}`} style={{color:'red'}}>
                   {b.name}
                 </Link>
               </li>
-            })
+            ))
+
           }
         </ul>
       </div>
